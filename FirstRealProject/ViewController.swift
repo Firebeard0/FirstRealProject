b8dd0c66a9bbd67e81c2e056387c41cf7b13a941
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-10-06 12:24:06 -0600

    Changed gitIgnored files

---
+++ FirstRealProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FirstRealProject
+//
+//  Created by Uffens, Noah on 10/6/17.
+//  Copyright © 2017 Uffens, Noah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
