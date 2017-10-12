e75eea12dd37afb027bf214f00570e48666027ad
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-10-12 13:11:30 -0600

    created a new controller

---
+++ FirstRealProject/New Group/SecondScreenController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondScreenController.swift
+//  FirstRealProject
+//
+//  Created by Uffens, Noah on 10/12/17.
+//  Copyright © 2017 Uffens, Noah. All rights reserved.
+//
+
+import UIKit
+
+public class SecondScreenController : UIViewController
+
